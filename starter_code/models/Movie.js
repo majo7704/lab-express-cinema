88ad84375eeab7dd39da42e14e2e18911dcef61e
j m <jm@js-MacBook-Pro.local>
2019-07-17 14:35:58 +0200

    set up of files

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = mongoose.model("movies", new Schema({
+  title: String,
+  director: String,
+  stars: {type: Array},
+  image: String,
+  description: String,
+  showtimes: {type: Array}
+}))
+
+module.exports = movieSchema
